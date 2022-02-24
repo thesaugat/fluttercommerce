890669b6927faaf8640a1392f7fe0663e739cf6c
Saugat Timilsina <thesaugatt@gmail.com>
2022-02-24 17:36:27 +0545

    grid added

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,8 +1,8 @@
-import 'dart:ui';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecom/utils/constants.dart';
 import 'package:ecom/utils/user_interface_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -35,7 +35,7 @@ class HomePage extends StatelessWidget {
         ),
         elevation: 1,
       ),
-      body: const FruitList(),
+      body: const HomeBody(),
     );
   }
 }
@@ -54,26 +54,124 @@ class _HomeBodyState extends State<HomeBody> {
     "https://ecommerce.saugatimilsina.com.np/public/slider_images/2021/12/1023493.jpeg",
   ];
 
+  final catImage = [
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+    "https://cdn.shopify.com/s/files/1/0586/6920/3620/products/iphone-13-pro-blue-select_a0c2135c-aaa7-49bf-b29d-19d900e62408_530x@2x.png",
+  ];
+  final catName = [
+    "#Microphone Audio",
+    "#Iphones",
+    "#Cables",
+    "#Powerbanks",
+    "#Bottle",
+    "#Laptop",
+    "#Sticker",
+    "#Helmets & Protection",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          child: SizedBox(
-            height: 170,
-            width: double.infinity,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              clipBehavior: Clip.antiAlias,
-              child: Image.network(
-                "https://ecommerce.saugatimilsina.com.np/public/slider_images/2021/12/1023493.jpeg",
-                fit: BoxFit.cover,
-              ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 150.0,
+              autoPlay: true,
+              viewportFraction: 1,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(milliseconds: 1500),
             ),
+            items: url.map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        clipBehavior: Clip.antiAlias,
+                        child: CachedNetworkImage(
+                          imageUrl: i,
+                          fit: BoxFit.fill,
+                        ),
+                      ));
+                },
+              );
+            }).toList(),
           ),
-        ),
-      ],
+          const SizedBox(
+            height: 14,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Categories",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w800),
+              ),
+              GestureDetector(
+                  child: const Text(
+                "SHOP MORE >",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                    color: kPrimaryColor),
+              ))
+            ],
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Container(
+            height: 230,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(.2),
+                borderRadius: BorderRadius.circular(10)),
+            child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: catName.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: MediaQuery.of(context).orientation ==
+                          Orientation.landscape
+                      ? 4
+                      : 4,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  childAspectRatio: (4 / 5),
+                ),
+                itemBuilder: (_, i) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 70,
+                          width: 70,
+                          child: CachedNetworkImage(imageUrl: catImage[i]),
+                        ),
+                        Text(
+                          catName[i],
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
